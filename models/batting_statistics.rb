69f400d88a6c8ecca69c98456fb6bb097a9d5567
stephan.com <stephan@stephan.com>
2014-08-06 22:45:58 -0700

    compute batting average

--- models/batting_statistics.rb
+++ models/batting_statistics.rb
@@ -1,5 +1,7 @@
 require_relative '../config/environment.rb'
 
 class BattingStatistics < ActiveRecord::Base
-
+  def batting_average
+    hits.to_f/at_bats
+  end
 end
--- spec/batting_statistics_spec.rb
+++ spec/batting_statistics_spec.rb
@@ -12,4 +12,14 @@ describe BattingStatistics do
   it { is_expected.to respond_to(:homers)     }
   it { is_expected.to respond_to(:rbi)        }
 
+  describe "batting average" do
+    it "should compute 0.5 for 50 hits and 100 at-bats" do
+      batter = create(:batting_statistics, at_bats: 100, hits: 50)
+      expect(batter.batting_average).to eq(0.5)
+    end
+    it "should compute 0.2 for 10 hits and 50 at-bats" do
+      batter = create(:batting_statistics, at_bats: 50, hits: 10)
+      expect(batter.batting_average).to eq(0.2)
+    end
+  end
 end
