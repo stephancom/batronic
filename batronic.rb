c43afaeecc357dac3d3ea11324f9ab57936e9228
stephan.com <stephan@stephan.com>
2014-08-07 05:17:35 -0700

    triple crown winners

--- batronic.rb
+++ batronic.rb
@@ -1,3 +1,9 @@
+#  _          _                _    
+# | |__  __ _| |_ _ _ ___ _ _ (_)__ 
+# | '_ \/ _` |  _| '_/ _ \ ' \| / _|
+# |_.__/\__,_|\__|_| \___/_||_|_\__|
+# stephan.com 2014
+
 require './config/environment.rb'
 require './models/batter'
 require 'ruby-progressbar'
@@ -49,7 +55,52 @@ def team_slugging(team, year)
   puts Terminal::Table.new(:rows => players.sort_by(&:last).reverse)
 end
 
+def triple_crown(league, year)
+  puts "Looking for triple crown winner in league #{league} for #{year}"
+  # TODO how to handle ties in one category or another?
+
+  crown_methods = [:batting_average, :homers, :rbi]
+  top_values = {}
+  top_keys = {}
+  # initialize hashes
+  crown_methods.each do |method|
+    top_values[method] = 0
+    top_keys[method] = []
+  end
+  # iterate through stats keeping track of winner in each category
+  BattingStatistic.where(year: year, league: league).each do |s|
+    next if s.at_bats < 400
+    crown_methods.each do |method|
+      value = s.send(method)
+      if value > top_values[method]
+        top_values[method] = value
+        top_keys[method] = [s.player_key]
+      elsif value == top_values[method]
+        top_keys[method] << s.player_key
+      end
+    end
+  end
+
+  if top_keys[:batting_average] == top_keys[:homers] and top_keys[:homers] == top_keys[:rbi]
+    puts "Winner found!"
+    puts Batter.find_by(player_key: top_keys[:rbi]).to_s
+  else
+    puts "(no winner)"
+  end
+end
+
+puts
+puts
 most_improved(2009, 2010, 200)
 puts
 puts
 team_slugging('OAK', 2007)
+puts
+puts
+[2011, 2012].each do |year|
+  %w(AL NL).each do |league|
+    triple_crown(league, year)
+    puts
+  end
+end
+puts
